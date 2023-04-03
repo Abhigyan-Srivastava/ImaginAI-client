ddec524b38d57a94602ec9c86bac2ae1bbafd125
Abhigyan-Srivastava <captainabhigyan.clasher@gmail.com>
2023-04-03 10:23:45 +0530

    Updated About Section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -142,19 +142,13 @@ const LandingPage = () => {
       <section className="sm:px-20 px-6 py-8">
         <div className="flex flex-col md:flex-row my-8 text-green_opaque mt-[4rem]">
           <div className="w-full md:w-6/12 flex flex-col justify-center items-center">
-            <svg
-              xmlns="/crul.svg"
-              fill="currentColor"
-              className="w-6/12"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-            </svg>
+          <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-medium font-sora">
+              About ImaginAI
+            </h2>
+          </div>
           </div>
           <div className="w-full md:w-6/12 flex flex-col justify-center my-6 md:my-0">
-            <p className="text-3xl sm:text-4xl md:text-5xl font-medium font-sora">
-              About ImaginAI
-            </p>
             <p className="text-slate md:pr-10 my-6 md:text-lg 2xl:text-xl font-poppins">
             Welcome to ImaginAI, the cutting-edge AI image generator website that lets you bring your creative vision to life. With our powerful AI algorithms and user-friendly interface, you can easily generate stunning images that are sure to impress.
             </p>
@@ -166,7 +160,7 @@ const LandingPage = () => {
             >
               Try ImaginAI&nbsp;
               <svg
-                xmlns="/crul.svg"
+                xmlns=""
                 fill="currentColor"
                 className="w-4 h-4 mr-2"
                 viewBox="0 0 16 16"
@@ -181,7 +175,7 @@ const LandingPage = () => {
       <section className="sm:px-20 px-6" id="faqsy">
         <div className="bg-lightblue py-20 px-4 text-zinc-500">
           <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
-            <h2 className="mr-8 w-full md:w-1/3 text-3xl font-bold leading-9 text-zinc-400 mb-10 md:mb-0 font-poppins">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-medium font-sora">
               Why to use ImaginAI?🤔
             </h2>
             <dl className="w-full md:w-2/3">
@@ -205,34 +199,10 @@ const LandingPage = () => {
         </div>
       </section>
 
-      {/* <section className="sm:px-20 px-6 py-8">
-        <div className="flex flex-col md:flex-row my-8 text-green_opaque mt-[4rem]">
-          <div className="w-full md:w-6/12 flex flex-col justify-center items-center">
-            <svg
-              // xmlns=""
-              fill="currentColor"
-              className="w-6/12"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-            </svg>
-          </div>
-          <div className="w-full md:w-6/12 flex flex-col justify-center my-6 md:my-0">
-            <p className="text-3xl sm:text-4xl md:text-5xl font-medium font-sora">
-              Turn your imaginations into creations🖼️
-            </p>
-            <p className="text-slate md:pr-10 my-6 md:text-lg 2xl:text-xl font-poppins">
-              Let us print your creation! We use Artificial Intelligence to enhance your image to print quality, manually crop it to the aspect ratio of your chosen size (making sure to keep the subject in-frame), then print and deliver it to your door.
-The prints are museum-quality, made on thick and durable matte paper. Add a wonderful accent to your room and office with these posters that are sure to brighten any environment.
-            </p>
-          </div>
-        </div>
-      </section> */}
-
       <section className="sm:px-20 px-6" id="faqs">
         <div className="bg-lightblue py-20 px-4 text-zinc-500">
           <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
-            <h2 className="mr-8 w-full md:w-1/3 text-3xl font-bold leading-9 text-zinc-400 mb-10 md:mb-0 font-poppins">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-medium font-sora">
               Frequently Asked Questions🙋
             </h2>
             <dl className="w-full md:w-2/3">
